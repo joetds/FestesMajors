8a7b32cb4d6d0de1006245bc00f36bada5c0db17
Joel Monné <joel.monne@gmail.com>
2016-03-08 12:54:24 +0100

    translations added

--- app/src/main/java/com/example/joelmonne/festesmajors/PaginaInicial.java
+++ app/src/main/java/com/example/joelmonne/festesmajors/PaginaInicial.java
@@ -68,6 +68,9 @@ public class PaginaInicial extends ActionBarActivity
             case 3:
                 mTitle = getString(R.string.title_section3);
                 break;
+            case 4:
+                mTitle = getString(R.string.title_section4);
+                break;
         }
     }
 
